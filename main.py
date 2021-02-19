baaf421d0db8bd2cdb80a17509b0bb00b5ead7b2
VSenna3202 <79334503+VSenna3202@users.noreply.github.com>
2021-02-19 16:45:53 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+import discord
+import asyncio
+
+client = discord.Client()
+
+
+#Comando para iniciar
+@client.event
+async def on_ready():
+    print(f'Bot {client.user.name} online\nHello world!')
+    print(f'id do bot: \n{client.user.id}')
+    await client.change_presence(activity=discord.Streaming(name='Eita', url='https://twitch.tv/Kl3t1n'))
+
+
+#Comando de texto e prefixo
+@client.event
+async def on_message(message):
+    if message.content.startswith('$'):
+        mention = message.author.mention
+        await message.channel.send(f'Bodia {mention}')
+
+
+
+#Comando para colocar o Token Do Bot
+client.run("ODEyMzgwMzczNTQzNTUxMDM3.YC_6Lg.Yro_HDRGDWWbju5GAjQuya9SIsc")
